6fd904c849df2bf322dae7aa441098e61096b2b4
nanaknihal <nanaknihal@gmail.com>
2022-07-23 20:42:22 -0700

    added files and express endpoint for alcc and alfa proofs

--- zk/prepareCircuit.sh
+++ zk/prepareCircuit.sh
@@ -1,4 +1,4 @@
 # boilerplate code to prepare a specific circuit for proof and verification
-# zokrates compile -i assertLeafContainsCreds.zok -o alcc
-# zokrates setup -i alcc -p alcc.proving.key -v alcc.verification.key
+# zokrates compile -i assertLeafContainsCreds.zok -o alcc.out
+# zokrates setup -i alcc.out -p alcc.proving.key -v alcc.verification.key
 # zokrates export-verifier -i alcc.verification.key -o ALCCVerifier.sol
