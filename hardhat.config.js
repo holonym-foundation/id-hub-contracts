c5d5d1e01d8c1ad3bd06c6e755b44198bfa76e48
calebtuttle <1calebtuttle@gmail.com>
2022-09-01 17:55:49 -0400

    linting

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('hardhat-abi-exporter');
+require("hardhat-abi-exporter");
 require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -26,13 +26,13 @@ module.exports = {
         version: "0.8.9",
       },
       {
-        version: '0.8.4'
-      }
-    ]
+        version: "0.8.4",
+      },
+    ],
   },
 
   abiExporter: {
-    path: './data/abi',
+    path: "./data/abi",
     runOnCompile: true,
     clear: true,
     flat: true,
@@ -44,23 +44,30 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      from : "0xC8834C1FcF0Df6623Fc8C8eD25064A4148D99388", // Send transactions from this address by default
-      accounts : [
-        {privateKey: process.env.PRIVATE_KEY, balance: "2110000000000000000000000"},
-        {privateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', balance: "10000000000000000000000"},
-        {privateKey: '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d', balance: "15100000000000000000000"},
-        
+      from: "0xC8834C1FcF0Df6623Fc8C8eD25064A4148D99388", // Send transactions from this address by default
+      accounts: [
+        { privateKey: process.env.PRIVATE_KEY, balance: "2110000000000000000000000" },
+        {
+          privateKey:
+            "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+          balance: "10000000000000000000000",
+        },
+        {
+          privateKey:
+            "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+          balance: "15100000000000000000000",
+        },
       ], // Private key for the above address
       forking: {
-        url: "https://xdai-archive.blockscout.com/", 
-        blockNumber: 21908151
-      }
+        url: "https://xdai-archive.blockscout.com/",
+        blockNumber: 21908151,
+      },
     },
     mumbai: {
       url: "https://speedy-nodes-nyc.moralis.io/a1167200f0a0e81dd757304e/polygon/mumbai",
       accounts: [process.env.PRIVATE_KEY],
       gas: 2100000,
-      gasPrice: 2000000000
+      gasPrice: 2000000000,
     },
     harmony: {
       url: "https://api.s0.b.hmny.io",
@@ -70,7 +77,7 @@ module.exports = {
       url: "https://rinkeby.arbitrum.io/rpc",
       accounts: [process.env.PRIVATE_KEY],
       gas: 2100000,
-      gasPrice: 2000000000
+      gasPrice: 2000000000,
     },
     avalanche: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
@@ -80,7 +87,7 @@ module.exports = {
       url: "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
       accounts: [process.env.PRIVATE_KEY],
       gas: 2100000,
-      gasPrice: 2000000000
+      gasPrice: 2000000000,
     },
     fantom: {
       url: "https://rpc.testnet.fantom.network",
@@ -91,11 +98,11 @@ module.exports = {
       url: "https://poa-xdai.gateway.pokt.network/v1/lb/60b13899d3279c22da2a444d",
       accounts: [process.env.PRIVATE_KEY],
       // gas: 10000000000,
-      gasPrice: 1500000000
+      gasPrice: 1500000000,
     },
-    arbitrumOne : {   
+    arbitrumOne: {
       url: "https://arb1.arbitrum.io/rpc",
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  }
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
