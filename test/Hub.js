49fdeb358ec1dc1420c0d30ede13db9061442c7a
nanaknihal <nanaknihal@gmail.com>
2022-09-19 16:09:11 -0400

    helper functions to create leaves

--- test/Hub.js
+++ test/Hub.js
@@ -1,6 +1,34 @@
 
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { readFileSync } = require("fs");
+const util = require("util");
+const { stdout, stderr } = require("process");
+const { randomBytes } = require("crypto");
+const exec = util.promisify(require("child_process").exec);
+
+// All inputs should be in field format (i.e., an integer modulo the 254-bit prime)
+const createLeaf = async (address, secret, countryCode, subdivision, completedAt, birthdate) => {
+  const r = randomBytes(16).toString("hex"); // Random bytes in the tmp filename so it won't be overwritten by another process runnign in parallel
+  const cliArgs = [address, secret, countryCode, subdivision, completedAt, birthdate].join(" ");
+  await exec(`zokrates compute-witness -a ${cliArgs} -i zk/compiled/createLeaf.out -o zk/tmp/${r}.tmp.witness`);
+  // Format output to just get witness:
+  return readFileSync(`zk/tmp/${r}.tmp.witness`).toString().split("\n")[0].split(" ")[1];
+}
+
+// All inputs should be in field format (i.e., an integer modulo the 254-bit prime)
+const createAdditionProof = async (address, countryCode, subdivision, completedAt, birthdate, oldSecret, newSecret) => {
+    const r = randomBytes(16).toString("hex"); // Random bytes in the tmp filename so it won't be overwritten by another process runnign in parallel
+    const oldLeaf = await createLeaf(address, oldSecret, countryCode, subdivision, completedAt, birthdate);
+    const newLeaf = await createLeaf(address, newSecret, countryCode, subdivision, completedAt, birthdate);
+    const cliArgs = [oldLeaf, newLeaf, address, countryCode, subdivision, completedAt, birthdate, oldSecret, newSecret].join(" ");
+    await exec(`zokrates compute-witness -a ${cliArgs} -i zk/compiled/onAddLeaf.out -o zk/tmp/${r}.tmp.witness`);
+    await exec(`zokrates generate-proof -i zk/compiled/onAddLeaf.out -w zk/tmp/${r}.tmp.witness -p zk/pvkeys/onAddLeaf.proving.key -j zk/tmp/${r}.proof.json`);
+    
+    return JSON.parse(readFileSync(`zk/tmp/${r}.proof.json`));
+}
+
+createAdditionProof(1, 2, 3, 4, 5, 6, 7).then(x=>console.log(x))
 
 describe("Hub", function () {
   describe("Signature", function () {
