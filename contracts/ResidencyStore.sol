f3deb0c3c82b67604ac187d45e1183a3ba3645d8
nanaknihal <nanaknihal@gmail.com>
2022-09-25 22:14:13 -0400

    proof of residency should no longer be vulnerable to malleability attacks

--- contracts/ResidencyStore.sol
+++ contracts/ResidencyStore.sol
@@ -18,9 +18,9 @@ contract ResidencyStore is Ownable  {
     /// @param proof PairingAndProof.sol Proof struct
     /// @param input The public inputs to the proof, in ZoKrates' format
     function prove(Proof calldata proof, uint[] calldata input) public onlyOwner {
-        console.log(input[0], input[1], input[2]);
-        require(input[1] == 1144726183143482297049508718223886886488380117896, "Proof must come from authority address"); // This is integer representation of the address 0xc88... 
-        require(input[2] == 2, "Credentials do not have US as country code"); // 2 is prime that represents USA because USA is #2
+        require(uint256(uint160(msg.sender)) == input[1], "Second public argument of proof must be your address");
+        require(input[2] == 1144726183143482297049508718223886886488380117896, "Proof must come from authority address"); // This is integer representation of the address 0xc88... 
+        require(input[3] == 2, "Credentials do not have US as country code"); // 2 is prime that represents USA because USA is #2
         require(hub.verifyProof("USResident", proof, input), "Failed to verify proof");
         usResidency[msg.sender] = true;
         emit USResidency(msg.sender, true);
