222d4d5b6d61f89be6d68ac03d40d3993a49c76b
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-01-15 15:23:48 -0500

    Roots.js now tested

--- test/Roots.js
+++ test/Roots.js
@@ -2,30 +2,23 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-// To Test:
-// only owner can add roots
-// owner can transfer ownership
-// after transferring ownership, previous owner cannot (this should almost definitely be covered by openzeppelin tests but if it's easy, might as well include for extra security)
-// ROOT_HISTORY_SIZE recent roots are stored in the list and the map
-// ROOT_HISTORY_SIZE+1th root is not stored in list, nor is it in the map
-// mostRecentRoot() returns the most recent root
+describe.only("Roots", function () {
+    before(async function() {
+        [this.account1, this.account2, this.account3] = await ethers.getSigners();
 
-describe("Roots", function () {
-    describe.only("Ownership", function () {
-        before(async function() {
-            [this.account1, this.account2, this.account3] = await ethers.getSigners();
-
-            this.roots = await (await ethers.getContractFactory("Roots")).deploy();
-        });
-
+        this.roots = await (await ethers.getContractFactory("Roots")).deploy();
+    });
+    
+    describe("Ownership", function () {
         it("Only owner can add roots", async function () {
             await expect(this.roots.connect(this.account1).addRoot(69)).to.not.be.reverted;
-            await expect(this.roots.connect(this.account2).addRoot(69)).to.be.revertedWith("Ownable: caller is not the owner");
-            
+            await expect(this.roots.connect(this.account2).addRoot(69)).to.be.revertedWith("Ownable: caller is not the owner"); 
         });
 
         it("Only owner can transfer ownership and afterwards, no longer is owner", async function () {
-            expect(0).to.equal(1);
+            await expect(this.roots.connect(this.account1).transferOwnership(this.account2.address)).to.not.be.reverted;
+            await expect(this.roots.connect(this.account1).addRoot(69)).to.be.reverted;
+            await expect(this.roots.connect(this.account2).addRoot(69)).to.not.be.reverted;
         });
     });
 
@@ -37,15 +30,46 @@ describe("Roots", function () {
         });
 
         it("ROOT_HISTORY_SIZE recent roots are stored in the list and the map", async function () {
-            expect(0).to.equal(1);
-        });
+            const MAX_ONCHAIN_HISTORY = await this.roots.ROOT_HISTORY_SIZE(); 
+            let allHistory = [];
+            // Go a bit over max history to ensure it correctly cycles:
+            for(i=0; i < MAX_ONCHAIN_HISTORY + 5; i++) {
+                let cyclicCounter = i % MAX_ONCHAIN_HISTORY;
+                const newRoot = ethers.BigNumber.from(ethers.utils.randomBytes(32));
+                allHistory.push(newRoot);
 
-        it("ROOT_HISTORY_SIZE+1th root is not stored in list, nor is it in the map", async function () {
-            expect(0).to.equal(1);
+                // // let mostRecentRoot = await this.mt.mostRecentRoot();
+                // if(i >= MAX_HISTORY) {
+                //     const old = await this.roots.recentRootsList(cyclicCounter);
+                //     expect(await this.roots.rootIsRecent(old)).to.equal(true);
+                //     await this.mt.insertLeaf(69);
+                //     const cur = await this.roots.recentRootsList(cyclicCounter);
+                //     expect(await this.roots.rootIsRecent(cur)).to.equal(true);
+
+                    
+                //     expect(await this.roots.rootIsRecent(old)).to.equal(false);
+                // } else {
+                //     await
+                // }
+                await this.roots.addRoot(newRoot);
+            }
+            // Check it doesn't have first root
+            expect(await this.roots.rootIsRecent(allHistory[0])).to.equal(false);
+            // Check it doesn't have fifth root
+            expect(await this.roots.rootIsRecent(allHistory[4])).to.equal(false);
+            // Check it has sixth root
+            expect(await this.roots.rootIsRecent(allHistory[5])).to.equal(true);
+            // Check it has second most recent root
+            expect(await this.roots.rootIsRecent(allHistory[allHistory.length-2])).to.equal(true);
+            // Check it has most recent root
+            expect(await this.roots.rootIsRecent(allHistory[allHistory.length-1])).to.equal(true);
+
         });
 
         it("mostRecentRoot() returns the most recent root", async function () {
-            expect(0).to.equal(1);
+            await this.roots.addRoot(123);
+            await this.roots.addRoot(569);
+            expect(await this.roots.mostRecentRoot()).to.equal(569);
         });
     });
 
