2e9d1c5c8e6c648fb2354e018d3ff7a1c7caf083
Nanak Nihal Singh Khalsa <nanaknihal@gmail.com>
2023-01-27 18:18:14 -0500

    removed console log

--- utils/proofs.js
+++ utils/proofs.js
@@ -15,7 +15,6 @@ async function createProofCircom(circuitName, zkeyName, inputs) {
 
 async function verifyProofCircom(circuitName, proof) {
     const vKey = JSON.parse(fs.readFileSync(`./zk/pvkeys/circom/${circuitName}_verification_key.json`));
-    console.log("vkey", vKey, "proof", proof.proof, "inputs", proof.inputs)
     return await snarkjs.groth16.verify(vKey, proof.publicSignals, proof.proof);
 }
 
